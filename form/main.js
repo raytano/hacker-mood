8fcb43c28a826001a647934c44f8d60cfd0c1c58
raytano <41718324+raytano@users.noreply.github.com>
2021-05-04 01:58:20 +0000

    Add files via upload

---
+++ form/main.js
@@ -0,0 +1,36 @@
+/* ======================================================================
+  Author Custom JavaScript
+====================================================================== */
+// Loop through Array of Objects
+
+var objPeople = [
+	
+	{ // Object @ 1 index
+		username: "raytano",
+		password: ""
+	},
+
+]
+
+function getInfo() {
+	var username = document.getElementById('username').value
+	var password = document.getElementById('password').value
+	
+
+	for(var i = 0; i < objPeople.length; i++) {
+		// check is user input matches username and password of a current index of the objPeople array
+		if(username == objPeople[i].username && password == objPeople[i].password) {
+            alert("welcome " + username)
+            window.location.replace("../chall/challenge.html")
+
+			
+
+			// stop the function if this is found to be tru
+				 
+				
+				
+			return
+			
+		}
+	}
+}
